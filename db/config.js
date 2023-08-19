297fcb806145f333574b03ed00acf69c61276b32
root <pabongamas@gmail.com>
2023-08-19 13:05:52 -0500

    config migractions npm scripts

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
-const {config}=require('./../../config/config');
+const {config}=require('./../config/config');
 
 const USER=encodeURIComponent(config.dbUser);
 const PASSWORD=encodeURIComponent(config.dbPassword);
